934990609dde010ad188be815b84d8107a7b71ae
Josue Rodriguez <josue.rodriguez594@gmail.com>
2024-11-16 19:59:41 -0800

    add helper script for symlinks

--- _make_symlink.r
+++ _make_symlink.r
@@ -1,6 +1,6 @@
 # Replace these paths with your actual paths
-home_path <- "/Users/josue/GitHub/econometrics/01_regression.qmd"
-quarto_path <- "/Users/josue/GitHub/josue-rodriguez.github.io/posts/2024-08-test/index.qmd"
+home_path <- "/Users/josue/GitHub/econometrics/02_matrix_ols.qmd"
+quarto_path <- "/Users/josue/GitHub/josue-rodriguez.github.io/posts/2024-11-11-matrix-ols/index.qmd"
 
 # Create the symbolic link
-file.symlink(from = quarto_path, to = home_path)
+file.symlink(from = home_path, to = quarto_path)
