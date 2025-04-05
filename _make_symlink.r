f74ccaa40b9d5b608208e21424946370ef15a78f
Josue Rodriguez <josue.rodriguez594@gmail.com>
2025-04-05 13:37:10 -0700

    prediction interval blog

--- _make_symlink.r
+++ _make_symlink.r
@@ -1,6 +1,6 @@
 # Replace these paths with your actual paths
-home_path <- "/Users/josue/GitHub/econometrics/02_matrix_ols.qmd"
-quarto_path <- "/Users/josue/GitHub/josue-rodriguez.github.io/posts/2024-11-11-matrix-ols/index.qmd"
+home_path <- "/Users/josue/GitHub/blogposts/02_matrix_ols.qmd"
+quarto_path <- "/Users/josue/GitHub/josue-rodriguez.github.io/posts/2024-11-16-matrix-ols/index.qmd"
 
 # Create the symbolic link
 file.symlink(from = home_path, to = quarto_path)
